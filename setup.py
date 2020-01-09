9b47d2a2969dd29f62301ebf02c44afca4209f3e
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-01-09 18:33:06 +0200

    init

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'yottos_shopify', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            msg = 'Cannot find version in yottos_shopify/__init__.py'
+            raise RuntimeError(msg)
+
+
+install_requires = [
+    'Django',
+    'django-background-tasks',
+    'mysqlclient',
+    'pyactiveresource',
+    'ShopifyAPI',
+    'mysqlclient'
+]
+
+setup(
+    name="Yottos-Shopify-App",
+    version=read_version(),
+    url="",
+    packages=find_packages(),
+    package_data={
+
+    },
+    install_requires=install_requires,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'manage = manage:main',
+        ],
+    }
+)
---
+++ yottos_shopify/app_settings_sample.py
@@ -0,0 +1,11 @@
+# Shopify app settings
+
+SHOPIFY_API_KEY = 'api key'
+SHOPIFY_API_SECRET = 'api secret key'
+SHOPIFY_API_SCOPE = ['read_script_tags', 'write_script_tags', 'read_products', 'read_orders', 'write_orders']
+SHOPIFY_API_VERSION = '2019-04'
+DB_NAME = 'db name'
+DB_USER = 'db user'
+DB_PASSWORD = 'db password'
+URL = 'app url'
+SECRET_KEY = 'django secret key'
