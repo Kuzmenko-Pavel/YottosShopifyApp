ec60788f7a110758a0ec92f28926060a961239b8
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-01-19 15:12:02 +0200

    create proxy app link

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # YottosShopifyApp
+
+- Create a Python virtual environment.
+
+    virtualenv --no-site-packages -p python3.5 env
+
+- Upgrade packaging tools.
+
+    env/bin/pip install --upgrade pip setuptools
+
+- Install the project in editable mode with its testing requirements.
+
+    env/bin/pip install -e "."
+    
+- Activate virtualenv in console
+
+    source env/bin/activate
+
+
+<a href="/collections/all?page=17">Next »</a>
---
+++ yottos_shopify/app_settings.py
@@ -0,0 +1,13 @@
+# Shopify app settings
+
+SHOPIFY_API_KEY = '5aedc5bc79f1ac9ea8bf45c8b786cec2'
+SHOPIFY_API_SECRET = 'f7b4a5856490a7355107167c4d462281'
+SHOPIFY_API_SCOPE = ['read_script_tags', 'write_script_tags', 'read_orders', 'write_orders']
+SHOPIFY_API_VERSION = '2019-04'
+DB_NAME = 'shopify'
+DB_USER = 'shopify'
+DB_PASSWORD = 'shopify'
+DB_HOST = 'srv-5.yottos.com'
+DB_PORT = '3306'
+URL = 'ef422c80.ngrok.io'
+SECRET_KEY = 'django secret key'
