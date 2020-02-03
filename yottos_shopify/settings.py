abfa8f0f856b4c326ea37887d84027c604cae157
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-02-03 16:38:51 +0200

    create proxy app link

--- yottos_shopify/settings.py
+++ yottos_shopify/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.CachedStaticFilesStorage"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "yottos_shopify/static"),
