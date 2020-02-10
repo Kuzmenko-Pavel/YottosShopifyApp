2fff92e85b16b298cad6584312db1a03857f870f
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-02-10 18:08:09 +0200

    add facebook analytic

--- app_disign/src/Feed.js
+++ app_disign/src/Feed.js
@@ -1,19 +1,33 @@
-import React, { Component } from "react";
-import {Card, Layout, TextField} from "@shopify/polaris";
+import React, {useCallback, useState} from "react";
+import {Banner, Button, Card, Layout, Link, Stack} from "@shopify/polaris";
 
-class Feed extends Component {
-    render() {
-        let url = 'https://' + this.props.current_shop.domain + '/a/ytt_feed/' + this.props.current_shop.feed.link + '.xml';
-            return (
-                <Layout.AnnotatedSection
-                    title={this.props.current_shop.feed.title}
-                    description={this.props.current_shop.feed.description}>
-                    <Card sectioned title={this.props.current_shop.feed.sectioned_title} actions={[{content: 'Copy to clipboard'}]}>
-                        <TextField type="text" labelHidden value={url} disabled/>
-                    </Card>
-                </Layout.AnnotatedSection>
-            );
-    }
+
+export default function Feed(props) {
+    const [active, setActive] = useState(false);
+    const handleChange = useCallback(() => {
+        if (!active) {
+            window.ga('send', 'event', 'Generate', 'Feed', props.current_shop.feed.link);
+            if (window.fbq) {
+                fbq('trackCustom', 'Generate_Feed_' + props.current_shop.feed.link);
+            }
+            setActive(!active);
+        }
+    }, [active]);
+    let url = 'https://' + props.current_shop.domain + '/a/ytt_feed/' + props.current_shop.feed.link + '.xml';
+    return (
+        <Layout.AnnotatedSection
+            title={props.current_shop.feed.title}
+            description={props.current_shop.feed.description}>
+            <Card sectioned title={props.current_shop.feed.sectioned_title}>
+                <Stack spacing="loose" vertical>
+                    {active ? <Banner>{props.current_shop.feed.sectioned_title} - {' '} <Link url={url}
+                                                                                              external={true}>{props.current_shop.feed.link + '.xml'}</Link>
+                    </Banner> : null}
+                    <Stack distribution="trailing">
+                        <Button onClick={handleChange}>Generate Feed</Button>
+                    </Stack>
+                </Stack>
+            </Card>
+        </Layout.AnnotatedSection>
+    );
 }
-
-export default Feed;
