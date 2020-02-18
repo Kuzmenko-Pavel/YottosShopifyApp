202462d225b26cfe100d477f0ffc457bd6df3747
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-02-18 16:12:52 +0200

    optimisation

--- shopify_app/views.py
+++ shopify_app/views.py
@@ -304,7 +304,7 @@ class Subscribe(TemplateView, BaseShop):
                         route_url('shopify_app:subscribe_submit', _query=_query))
                     rac.price = 29.00
                     rac.trial_days = 60
-                    rac.trial_ends_on = timezone.now() + timezone.timedelta(days=60)
+                    # rac.trial_ends_on = timezone.now() + timezone.timedelta(days=60)
                     rac.name = "Premium"
                     if rac.save():
                         context['url'] = rac.confirmation_url
