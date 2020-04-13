bcdea4d08f3028772a2dd53ea5883390efc3a014
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-04-13 15:49:12 +0300

    add analytyc event

--- facebook_app/admin.py
+++ facebook_app/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import FacebookBusinessManager, FacebookCampaign, FacebookFeed
+
+admin.site.register(FacebookBusinessManager)
+admin.site.register(FacebookCampaign)
+admin.site.register(FacebookFeed)
