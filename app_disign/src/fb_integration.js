8b464c435d457019267aff17d614469686262892
Kuzmenko Pavel <kyzmenko.pavel@gmail.com>
2021-12-06 11:05:49 +0200

    change fb api version

--- app_disign/src/fb_integration.js
+++ app_disign/src/fb_integration.js
@@ -115,7 +115,7 @@ window.fbAsyncInit = function () {
         cookie: true,
         xfbml: false,
         status: true,
-        version: 'v8.0'
+        version: 'v12.0'
     });
     connectFb()
 };
